0a57c16d317b51d15b4d9d1a610dba5f41c0ee7a
Nickkos <Nickkoskowski@gmail.com>
2015-12-11 21:49:28 -0800

    Added initial files

---
+++ html/brands.php
@@ -0,0 +1,65 @@
+<?php include 'header.php'; ?>
+<main>
+	<div class="container">
+		<div id="breadcrumbs">
+			<div class="row">
+				<div class="col-md-12">
+					<p><a href="">Home</a> > <a href="">Brands</a></p>
+				</div>
+			</div>
+		</div>
+		<div id="brands">
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/logos/coachmen.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/american-coach.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/fleetwood.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/forest-river.png">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/logos/gulf-stream.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/highland-ridge.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/holiday-rambler.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/little-guy.png">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/logos/newmar.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/primetime.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/roadtrek.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/rockwood.png">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/logos/sandstorm.png">
+				</div>
+				<div class="col-md-3">
+					<img src="images/logos/workandplay.png">
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<?php include 'footer.php'; ?>
---
+++ html/footer.php
@@ -0,0 +1,7 @@
+	<footer>
+		<div class="container">
+			<p>(XXX)-XXX-XXXX - Copyright &copy; 2015 All Rights Reserved</p>
+		</div>
+	</footer>
+</body>
+</html>
